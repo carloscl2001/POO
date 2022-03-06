a0b7fbcc555664b444e25edfb5658429855b5881
carloscl2001 <carloscorteslora@gmail.com>
2022-03-06 17:51:40 +0100

    1-2-3

--- Cadena.cpp
+++ Cadena.cpp
@@ -1,1 +1,8 @@
+#include "Cadena.hpp"
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+//CONSTRUCTORES
+Cadena::Cadena(size_t t = 0, char c): s_(new char[])
--- Cadena.hpp
+++ Cadena.hpp
@@ -1,1 +1,27 @@
+#ifndef CADENA_HPP_
+#define CADENA_HPP_
 
+
+class Cadena
+{
+    public://=>métodos
+        
+        //CONSTRUCTORES
+        Cadena(size_t s = 0, char c = ' ');
+        Cadena(const Cadena& cadena);
+
+
+        //OBSERVADOR
+        inline size_t length() const {return tam_;}
+
+        //OPERADORES
+
+    private://=>atribitos
+
+        char *s_;
+        size_t tam_;
+
+        //FUNCIONES PRIVADAS AUXILIARES
+};
+
+#endif

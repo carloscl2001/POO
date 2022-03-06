06bc783856c677ef34b643500176ab308d53912b
carloscl2001 <carloscorteslora@gmail.com>
2022-03-06 18:55:23 +0100

    j

--- Cadena.cpp
+++ Cadena.cpp
@@ -1,8 +1,12 @@
 #include "Cadena.hpp"
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
 //CONSTRUCTORES
-Cadena::Cadena(size_t t = 0, char c): s_(new char[])
+Cadena::Cadena(size_t t = 0, char c): s_(new char[t + 1]), tam_(t)
+{
+
+}

8f209490ee35ae7eb1d26ce315018d956e28a086
carloscl2001 <carloscorteslora@gmail.com>
2022-03-30 19:38:01 +0200

    g

--- Examenes resueltos/MatrizDispersa.hpp
+++ Examenes resueltos/MatrizDispersa.hpp
@@ -22,4 +22,6 @@ class  MatrizDispersa
         inline size_t filas() const noexcept {return m;}
         inline size_t columnas() const noexcept {return n;}
         size_t n_valores() const;
+
+        bool operator < (const terna& t);
 };

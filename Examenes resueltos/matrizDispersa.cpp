5711d6ae151c58bb22d5c59588f2190dbaa6653f
carloscl2001 <carloscorteslora@gmail.com>
2022-03-30 19:33:57 +0200

    f

--- Examenes resueltos/matrizDispersa.cpp
+++ Examenes resueltos/matrizDispersa.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <vector>
+
+class  MatrizDispersa
+{
+    private:
+        struct terna
+        {
+            size_t f, c;
+            double v;
+        };
+        
+        size_t m, n;
+        std::vector<terna> val;
+
+        bool buscar(size_t fil, size_t col, size_t& indice);
+
+    public:
+        explicit MatrizDispersa(int f = 1, int c = 1);
+        void asignar();
+        double valor() const;
+        inline size_t filas() const noexcept {return m;}
+        inline size_t columnas() const noexcept {return n;}
+        size_t n_valores() const;
+};

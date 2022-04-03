5edb796da6348da5da46b0046ede4090b867f6c3
carloscl2001 <carloscorteslora@gmail.com>
2022-04-03 20:57:51 +0200

    gdfgdfh

--- Examenes resueltos/1Parcial2015.cpp
+++ Examenes resueltos/1Parcial2015.cpp
@@ -95,7 +95,7 @@ double a = 1.5, b= 10.5;
 doble c = 5.25,
 d=c;
 double e = 4;
-e = a;
+e = c;
 std::cout << c / d * b + b * c - c * c / b + b / c << std::endl; //65
 d = a = b += c;
 std::cout << d << ' ' << a << ' ' << b << ' '<< c << ' ' << e <<std::endl; // 15,75 15,75 15,75 5,25

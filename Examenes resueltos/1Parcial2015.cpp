21057bd23fb5fcd56ea4f2b116173e190b0fb237
carloscl2001 <carloscorteslora@gmail.com>
2022-04-03 20:55:31 +0200

    jkljklj

--- Examenes resueltos/1Parcial2015.cpp
+++ Examenes resueltos/1Parcial2015.cpp
@@ -90,5 +90,13 @@ class doble
 
 doble::doble(double n): d(n){}
 
-
-
+int main(){
+double a = 1.5, b= 10.5;
+doble c = 5.25,
+d=c;
+double e = 4;
+e = a;
+std::cout << c / d * b + b * c - c * c / b + b / c << std::endl; //65
+d = a = b += c;
+std::cout << d << ' ' << a << ' ' << b << ' '<< c << ' ' << e <<std::endl; // 15,75 15,75 15,75 5,25
+}

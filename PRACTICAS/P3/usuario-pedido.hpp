c3174b3bfb9a1e523ed5f0cdcbbc959fa6512de3
carloscl2001 <carloscorteslora@gmail.com>
2022-05-08 16:17:49 +0200

    jk;

--- PRACTICAS/P3/usuario-pedido.hpp
+++ PRACTICAS/P3/usuario-pedido.hpp
@@ -1,14 +1,35 @@
 #ifndef USUARIO_PEDIDO_HPP
 #define USUARIO_PEDIDIO_HPP
-class Usuario_Pedido
-{
+
+#include <map>
+#include <set>
+#include "usuario.hpp"
+#include "pedido.hpp"
+
+class Usuario_Pedido{
     private:
-        
+        typedef std::map<Usuario*, std::set<Pedido*>> usu_ped_;
+        typedef std::map<Pedido*, Usuario*> ped_usu_
+
     public:
+        void asocia(Usuario& usu, Pedido& ped);
+        void asocia(Pedido& ped, Usuario& usu);
+
+        Pedidos pedidos(Usuario& usu) {return usu_ped[&usu];}
+        Usuario* cliente(Pedido& ped) {return ped_usu[&ped];}
 };
 
+inline  void asocia(Usuario& usu, Pedido& ped)
+{
+    usu_ped_[&usu].insert(&ped);
+    ped_usu_[&ped] = &user;
 
+}
 
+inline  void asocia(Pedido& ped, Usuario& usu)
+{
+    asocia(usu,ped);
+}
 
 
 

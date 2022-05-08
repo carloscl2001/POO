a0c182ddf52e9c9940ec8084b47cbd9d061b9e45
carloscl2001 <carloscorteslora@gmail.com>
2022-05-08 17:11:37 +0200

    lolaso

--- PRACTICAS/P3/pedido.hpp
+++ PRACTICAS/P3/pedido.hpp
@@ -1,7 +1,18 @@
 #ifndef PEDIDO_HPP
 #define PEDIDO_HPP
 
+#include "fecha.hpp"
+#include "numero.hpp"
+
 class Pedido{
+    public:
+
+    private:
+        Tarjeta* tarjeta_;
+        Fecha fecha_pedido_;
+        unsigned importe_total_;
+        Numero num_pedido_p;
+
 
 };
 

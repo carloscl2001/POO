c975d84931dc3dc93732137a4b674e8786df05f9
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-01-22 23:38:31 +0100

    dfdfd

--- EXAMENES  RESUELTOS/Febrero/MatrizDispersa.cpp
+++ EXAMENES  RESUELTOS/Febrero/MatrizDispersa.cpp
@@ -1,16 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <sort>
+
+using namespace std;
 class MatrizDispersa
 {
-private:
-    /* data */
-public:
-    MatrizDispersa(/* args */);
-    ~MatrizDispersa();
+    public:
+        explicit MatrizDispersa(size_t n1 = 1, size_t n2 = 1):n(n1),m(n2){}
+        void asignar(size_t pos1, size_t pos2, double valor);
+        double valor(size_t pos1, size_t pos2) const;
+        size_t filas() const{return n;}
+        size_t columnas() const{return m;}
+        size_t n_valores() const;
+        bool buscar(size_t pos1, size_t pos2, size_t& indice);
+        
+    private:
+        struct terna{
+            size_t f,c;
+            double v;
+            bool operator<(const terna& t1, const terna& t2);
+        };
+        size_t n;
+        size_t m;
+        std::vector<terna>val;
 };
 
-MatrizDispersa::MatrizDispersa(/* args */)
-{
+void MatrizDispersa::asignar(size_t pos1, size_t pos2, double valor){
+    int i = 0;
+    for(auto it = val.begin(); it != val.end(); it++){
+        if(it->f == pos1 && it->c == pos2)
+            val[i].v = valor;
+        else{
+            terna t;
+            t.f = pos1;
+            t.c = pos2;
+            t.v = valor;
+            val.insert(it, t);
+        }
+        i++;
+    }
+    val.sort([](terna t1, terna t2) -> bool{return (t1 < t2);});
 }
 
-MatrizDispersa::~MatrizDispersa()
-{
+double MatrizDispersa::valor(size_t pos1, size_t pos2) const{
+    int i = 0;
+    for(auto it = val.begin(); it != val.end(); it++){
+        if(it->f == pos1 && it->c == pos2)
+            return val[i].v;
+        i++;
+    }
+}
+
+size_t MatrizDispersa::n_valores() const{
+    size_t i = 0;
+    for(auto it = val.begin(); it != val.end(); it++)
+        i++;
+    return i;
+}
+
+bool MatrizDispersa::buscar(size_t pos1, size_t pos2, size_t& indice){
+    size_t i = 0;
+    terna t;
+    t.f = pos1;
+    t.c = pos2;
+    while(indice < val.size() && val[i] < t){
+        if(it->f == pos1 && it->c == pos2){
+            return true;
+            indice = i;
+        }
+        else{
+            if(i == n_valores())
+                indice = n_valores();
+            else
+                indice = i
+            return false;
+            
+        }
+        i++;
+    }
+}
+
+friend bool MatrizDispersa::terna::operator <(const terna& t1, const terna& t2){
+    if(f < t2.f) return true;
+    if(f == t2.f && c < t2.c) return true;
+
+    return false;
+
 }
--- EXAMENES  RESUELTOS/PARCIAL 1/MatrizDispersa.cpp
+++ EXAMENES  RESUELTOS/PARCIAL 1/MatrizDispersa.cpp
@@ -1,21 +1,121 @@
-#include "MatrizDispersa.hpp"
+#include  <iostream>
+#include <initializer_list>
+#include <vector>
+#include <list>
+#include <sort>
 
-bool MatrizDispersa::operator <(const terna& t)
-{
-    if(f < t.f ) return true;
-    if(f == t.f && c < c.f) return true;
-    else return false;
+using namespace std;
+
+class MatrizDispersa{
+    public:
+        typedef struct terna;
+        explicit MatrizDispersa(size_t m=1, size_t n=1);
+        void asignar(size_t fil, size_t col, double valor);
+        MatrizDispersa(const initializer_list<terna>& l);
+        double valor(size_t fil, size_t col ) const;
+        size_t filas() const noexcept;
+        size_t columnas() const noexcept;
+        size_t n_valores() const noexcept;
+
+
+        friend void intercambiar(MatrizDispersa& A, MatrizDispersa& B);
+        inline double& valor(size_t indice) {return val[indice].v;} //esto faltaria el out of range 
+        
+    private:
+        bool buscar(size_t fil, size_t col, size_t& indice);
+
+        struct terna{
+            size_t f,c;
+            double v;
+            friend bool operator <(const terna& t1, const terna& t2);
+        };
+        size_t m,n;
+        vector<terna> val;
+};
+/*double MatrizDispersa::valor(size_t indice)const{
+    if(indice>val.size()) throw out_of_range("Indice fuera de rango");
+    return val[indice].v;
+}*/
+
+size_t MatrizDispersa::filas() const noexcept{
+    return m;
 }
 
-bool MatrizDispersa::buscar(size_t fil, size_t col, size_t& indice)
-{
-    for(val[0][0]; val[c][f]; c++, f++ )
-    {
-        if(val[c][f] != 0){
-            return true;
-        }
-        else{
-        }
-            
+size_t MatrizDispersa::columnas() const noexcept{
+    return n;
+}
+
+size_t MatrizDispersa::n_valores() const noexcept{
+    return val.size();
+}
+
+MatrizDispersa::MatrizDispersa(const initializer_list<terna>& l){
+    initializer_list<terna>::iterator it;
+    for(it= l.begin(); it != l.end()-1;it++){
+        val.push_back((*it));
+        it++;
     }
+    it++;
+    if(it->v != 0) val.push_back(*it);
+
+    m = it->f;
+    n = it->c;
+}
+
+int main(){ //suponemos que terna esta en public pq sino no se puede hacer
+    MatrizDispersa a(5,4);
+    a.asignar(3,0,18.2);
+    //a.asignar().....;
+    //o se podria hacer esto
+    //Matriz dispersa a={terna(0,1,7.5),terna(2,0,18.2)} ...
+    try{
+        a.asignar(8,6,0);
+    }catch(out_of_range& e){
+        cout<<"El programa se ha interrumpida por el siguiente motivo: " << e.what() << endl;
+    }
+}
+void intercambiar(MatrizDispersa& A, MatrizDispersa& B){
+
+}
+
+friend bool MatrizDispersa::terna::operator <(const terna& t1, const terna& t2){
+    if(f < t2.f) return true;
+    if(f == t2.f && c < t2.c) return true;
+
+    return false;
+
+}
+
+bool MatrizDispersa::buscar(size_t fil, size_t col, size_t& indice){
+    int ind = 0;
+
+    terna aux; aux.f = fil; aux.c = col;
+
+    while(ind < val.size() && val[ind] < aux){
+            if(val[ind].f == fil && val[ind].c == col){
+                indice = ind;
+                return true;
+            }
+            ind++;
+    }
+    if(ind == val.size()){
+        indice = n_valores();
+        return false;
+    }else{
+        indice = ind;
+        return false;
+    }
+}
+
+inline MatrizDispersa::MatrizDispersa(size_t m=1, size_t n=1): m(n), n(n){}
+
+void MatrizDispersa::asignar(size_t fil, size_t col, double valor){
+    if(fil >= m || col >= n) throw out_of_range("Fuera de rango");
+
+    terna aux; aux.f = fil; aux.c = col;aux.v = valor;
+    val.push_back(aux);
+    //list<terna>  l;
+    //copy(val.begin(),val.end(), l.begin());
+    
+    sort(val.begin(),val.end(), [](terna t1, terna t2) -> bool{return t1 < t2;});
 }

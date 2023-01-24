5b045c042aaf3a1cfa7899b2d262807ce6e4dc5f
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-01-24 19:20:51 +0100

    dsfsdfs

--- EXAMENES  RESUELTOS/Febrero/prueba.cpp
+++ EXAMENES  RESUELTOS/Febrero/prueba.cpp
@@ -1,32 +1,18 @@
  #include <iostream>
  #include <math.h>
+ #include <vector>
 using namespace std;
 class Complejo {
         double real, imag;
     public:
-        Complejo (double r = 0., double i = 0.): real(r), imag(i) {}
-        Complejo (const Complejo& c) {real = c.real; imag = c.imag;}
-        void print() { cout << "(" << real << "," << imag << ")"; }
-        // operator double() const { return sqrt(real * real + imag * imag); }
-        friend Complejo operator +(const Complejo& c1, const Complejo& c2);
-        friend Complejo operator -(const Complejo& c1, const Complejo& c2);
-        friend Complejo operator *(Complejo& c1, Complejo& c2);
+        Complejo (double r = 0 , double i = 0.): real(r), imag(i) {}
+        double real_() const{return real;}
 };  
-Complejo operator +(const Complejo& c1, const Complejo& c2) {
-    return Complejo(c1.real + c2.real, c1.imag + c2.imag);
-}
-Complejo operator -(const Complejo& c1, const Complejo& c2) {
-    return Complejo(c1.real - c2.real, c1.imag - c2.imag);
-}
-Complejo operator *(Complejo& c1, Complejo& c2) {
-    return Complejo(c1.real * c2.real - c1.imag * c2.imag,c1.real * c2.imag + c2.real * c1.imag);
-}
+
 
 int main() {
-Complejo a(3.);
-Complejo b(2., 2.);
-Complejo c = a + b;
-Complejo d = b + 3.;
-Complejo e = 3. + b;
-cout << "a * b = ", (a * b).print();
+vector<Complejo>vectorcito (100);
+
+cout<<vectorcito[20].real_();
+
 }

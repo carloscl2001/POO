9536410f4e2111cdd651d95c97c692784d529ab1
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-01-24 20:54:31 +0100

    dfbcvbcvb

---
+++ EXAMENES  RESUELTOS/Febrero/junio22.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string.h>
+using namespace std;
+
+class Cliente{
+    public:
+
+    private:
+        string cod;
+        string nombre;
+};

849094ef8ad867907f4e60e20d860d87d307efed
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-06-09 18:02:27 +0200

    Update mayo2008.cpp

--- 3MATRICULA/mayo2008.cpp
+++ 3MATRICULA/mayo2008.cpp
@@ -1,11 +1,38 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
 class A{};
-
-class B: public A{};
-
-class C{};
 
 class D{};
 
-class E: public A{};
+class B: public A{
+    private:
+        set<D*> d_;
+    public:
+        void rel3(D& d);
+};
+
+class C{
+    private:
+        D d;
+    public:
+
+};
+
+class D{
+    private: 
+        B* b;
+    public:
+        void rel3(B& b);
+};
+
+class E: public A{
+    private:
+        set<D*> d_;
+    public:
+        void rel5(D& d);
+};
 
 class F: public C, public E{};

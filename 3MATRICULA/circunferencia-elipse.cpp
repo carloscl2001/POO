299d712dbb394c59e8ee11b99af0f6ee95f5a0c3
carloscl2001 <99760570+carloscl2001@users.noreply.github.com>
2023-06-10 23:42:10 +0200

    JAJAJAJA

--- 3MATRICULA/circunferencia-elipse.cpp
+++ 3MATRICULA/circunferencia-elipse.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+//a)
+//La opción mas conveniente sería la especialiazción, ya que como nos dice en el enunciado 
+//una circunferencia es un tipo de elipse.
+
 class Elipse{
     public:
         Elipse(float f1, float f2);
         float radio_x() const;
         float radio_y() const;
-        virtual void escala(float escala);
+        void escala(float escala);
+
+    protected:
         void escala(float escala1, float escala2);
+        
     private:
         float radiox;
         float radioy;
@@ -26,18 +36,51 @@ void Elipse::escala(float escala1, float escala2){
     radioy *= escala2;
 }
 
+//
+
 class Circunferencia: public Elipse{
     public:
         Circunferencia(float radio);
-        void escalar(float escala);
     private:
 };
 
-Circunferencia::Circunferencia(float radio){
+Circunferencia::Circunferencia(float radio):Elipse(radio,radio){}
+*/
 
-}
+//b)]
+//Vamos a usar una clase abstracta en vez de una interfaz, ya que tenemos atributos y  métodos en común
+class FiguraPlana{
+    public:
+        FiguraPlana(float f1, float f2):radiox(f1), radioy(f2){}
+        void escala(float escalado){
+            radiox *= escalado;
+            radioy *= escalado;
+        }
+        float radio_x() const{return radiox;}
+        float radio_y() const{return radioy;}
 
+        virtual void dibuja() const = 0;
+    private:
+        float radiox;
+        float radioy;
+};
 
+class Elipse: public FiguraPlana{
+    public:
+        Elipse(float f1,float f2):FiguraPlana(f1,f2){}
+        virtual void dibuja() const{
+            cout<<"Dibujando una elipse"<<endl;
+        }
+};
 
-//La opción mas conveniente sería la especialiazción, ya que como nos dice en el enunciado 
-//una circunferencia es un tipo de elipse.
+class Circuferencia: public Elipse{
+    public:
+        Circuferencia(float f):Elipse(f,f){}
+        void dibuja() const{
+            cout<<"Dibujando una circunferencia"<<endl;
+        }
+        
+        float radio() const{
+            return radio_x();
+        }
+};
---
+++ 3MATRICULA/vehiculos.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//a)
+
+class Vehiculo{
+    public:
+        Vehiculo(string fecha):fecha_fabricacion(fecha){}
+        string fecha() const{return fecha_fabricacion;}
+        virtual void imprimir() const = 0;
+    private:
+        string fecha_fabricacion;
+};
+
+
+class Turismo: public Vehiculo{
+    public:
+        Turismo(string f, size_t p):Vehiculo(f),num_max_pasajeros(p){}
+        size_t pasajeros() const{return num_max_pasajeros;}
+        void imprimir() const{
+            cout<<num_max_pasajeros<<fecha()<<endl;
+        }
+    private:
+        size_t num_max_pasajeros;
+};
+
+class Camion: public Vehiculo{
+    public:
+        Camion(string f, size_t p):Vehiculo(f), peso_max_autorizado(p){}
+        size_t peso() const{return peso_max_autorizado;}
+        void imprimir() const{
+            cout<<peso_max_autorizado<<fecha()<<endl;
+        }
+    private:
+        size_t peso_max_autorizado;
+};
+
+
+
+
+//b)
+
+
+void clasificarVehiculos(const vector<Vehiculo*>& vehiculos, vector<Turismo*>& turismos, vector<Camion*>& camiones) {
+    for (vector<Vehiculo*>::const_iterator it = vehiculos.begin(); it != vehiculos.end(); ++it) {
+        if (Turismo* turismo = dynamic_cast<Turismo*>(*it)) {
+            turismos.push_back(turismo);
+        } else if (Camion* camion = dynamic_cast<Camion*>(*it)) {
+            camiones.push_back(camion);
+        }
+    }
+}
+
+int main(){
+    // Crear algunos objetos de vehículos
+    Turismo turismo1("01/01/2022", 4);
+    Turismo turismo2("01/02/2022", 2);
+    Camion camion1("01/03/2022", 5000);
+    Camion camion2("01/04/2022", 7000);
+
+    // Crear un vector de punteros a vehículos y agregar los objetos
+    vector<Vehiculo*> vehiculos;
+    vehiculos.push_back(&turismo1);
+    vehiculos.push_back(&turismo2);
+    vehiculos.push_back(&camion1);
+    vehiculos.push_back(&camion2);
+
+    // Clasificar los vehículos en vectores separados
+    vector<Turismo*> turismos;
+    vector<Camion*> camiones;
+    clasificarVehiculos(vehiculos, turismos, camiones);
+
+    // Imprimir los turismos
+    cout << "Turismos:" << endl;
+    for (Turismo* turismo : turismos) {
+        turismo->imprimir();
+    }
+}
+
+
